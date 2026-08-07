baf75965357071587f4eb8e0a74af487b775cea0
何俊浩 <240200079@mail.szpu.edu.cn>
2026-08-08 04:37:26 +0800

    style: refine desktop toolbox visual system

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './styles.css'
+import './styles.refined.css'
 import 'katex/dist/katex.min.css'
 
 createApp(App).use(createPinia()).use(router).mount('#app')
